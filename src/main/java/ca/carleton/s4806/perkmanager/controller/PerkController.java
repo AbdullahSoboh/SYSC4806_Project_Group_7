df4d7fd45a6664a67c69e2b8e683c1a3e8beb53c
Imann Brar <122045366+ImannBrar@users.noreply.github.com>
2025-11-02 13:38:49 -0500

    Import statements changed

--- src/main/java/ca/carleton/s4806/perkmanager/controller/PerkController.java
+++ src/main/java/ca/carleton/s4806/perkmanager/controller/PerkController.java
@@ -3,10 +3,12 @@ package ca.carleton.s4806.perkmanager.controller;
 import ca.carleton.s4806.perkmanager.model.Perk;
 import ca.carleton.s4806.perkmanager.repository.PerkRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;  // <-- add PostMapping, RequestBody, ResponseStatus
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;

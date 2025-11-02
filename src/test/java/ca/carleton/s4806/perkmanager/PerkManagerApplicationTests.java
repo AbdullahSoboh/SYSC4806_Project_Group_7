38ce637fbf6aef145cb384536f4232a99e62f3a1
Abdullah Soboh <abdullahsoboh@cmail.carleton.ca>
2025-11-02 14:09:08 -0500

    Added back deleted tests by accident

---
+++ src/test/java/ca/carleton/s4806/perkmanager/PerkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.carleton.s4806.perkmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerkManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
